34ac8b17d9332850e44b74dbb98b02ef5cf6bf2a
habibmulia <muliahabib1@gmail.com>
2022-04-05 21:43:01 +0700

    update pertemuan 4

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,22 +2,22 @@
     <h1><?php echo $judul ?></h1>
     <h4>Nama</h4>
     <ul type="disc">
-        <li>Nama Depan : Imam</li>
-        <li>Nama Belakang : Nawawi</li>
+        <li>Nama Depan : Muhammad Habib</li>
+        <li>Nama Belakang : Mulia</li>
     </ul>
     <br>
     <h4>Alamat</h4>
     <ul type="none">
-        <li> Jalan Ciledug Raya No. 168 Pesanggrahan</li>
+        <li> Jl. Danau Sunter Utara Blok M No.58-59</li>
     </ul>
 
     <h4>Tempat Lahir</h4>
     <ul type="none">
-        <li>Cirebon</li>
+        <li>Jakarta</li>
     </ul>
     <h4>Olah Raga Favorit</h4>
     <ul type="square">
         <li>Bulutangkis</li>
-        <li>Catur</li>
+        <li>Basket</li>
     </ul>
 </section>
